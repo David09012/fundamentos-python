0804701e190533c2c0f8ed0290d36c2bc8351e05
David Aguilar <david.aguilar@keyisntitute.edu.sv>
2025-05-30 14:56:08 -0600

    Tarea 9 ejercicio 1

--- 6.3.1.py
+++ 6.3.1.py
@@ -1,3 +1,13 @@
+'''
+Clase:        Clase 6
+Tema:         Manejo de lista
+Ejercicio:    Eliminando valores duplicados
+Descripción:  Le pido un conjunto de numeros al usuario, lo convierto en lista con espacios, creo una lista vacia, y comparo los la lista vacia con la lista del usuario, si el numero no esta en la lista vacia, lo agregra, y si esta, no hace nada.
+
+Autor:        David Alejandro Aguilar Aguilar
+Fecha:        2025-05-30
+Estado:       [ Terminado ]
+'''
 x=input("Dame una lista de numeros: ")
 lista= x.split()
 z=[]
